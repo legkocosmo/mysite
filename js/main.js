58686f7d44eac9aa843b071ff778a0ea954b11c8
legkocosmo <mylegko@gmail.com>
2020-08-21 11:26:26 +0300

    new block slide

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 
-  new WOW({
-    offset: 100,
-    mobile: true
-  }).init();
+new WOW({
+  offset: 100,
+  mobile: true
+}).init();
+
+var mySwiper = new Swiper('.swiper-container', {
+  speed: 400,
+  spaceBetween: 100,
+  loop: true,
+  pagination: {
+    el: '.projects-pagination',
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true
+  }
+});
+
+
